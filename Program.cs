0639eb5a5c4c14399a56f001acaf647b9b9ab9df
srlopez <srlopezh@gmail.com>
2021-10-23 09:00:47 +0200

    Comentarios

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 /*
 PRESENTACION DEL LENGUAJE
-
 */
 
 // Diseño de los datos
@@ -10,3 +9,6 @@ var so = new[] { 'H', 'M', 'H', 'M', 'M', 'H', 'H' };
 var ns = new[] { 7.5M, 4, 6, 5, 4, 6.5M, 7.5M };
 
 var n = ns.Length;
+
+// IF, GOTO, LABEL
+// Instrucciones de primer nivel

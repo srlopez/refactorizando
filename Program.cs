90a612ed2d980b93366af9a61bc966aa4924f720
srlopez <srlopezh@gmail.com>
2021-10-23 09:11:49 +0200

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
-PROGRAMACION IMPERATIVA
+PROGRAMACION ESTRUCTURADA EN PPROCEDIMIENTOS
 con las siguientes instrucciones exclusivamente IF, GOTO y LABEL
 
 Calcula la media del array, los participanntes, 
@@ -18,50 +18,63 @@ using System;
 var ne = new[] { "Luis", "Marta", "Marcos", "Aroa", "Nerea", "Kike", "Juan" };
 var so = new[] { 'H', 'M', 'H', 'M', 'M', 'H', 'H' };
 var ns = new[] { 7.5M, 4, 6, 5, 4, 6.5M, 7.5M };
+// DECLARACION 
 decimal x;
-var s = 0M;
-var m = 0M;
-var i = 0;
-var ph = 0;
-var pm = 0;
-var ah = 0;
-var am = 0;
-var sh = 0M;
-var sm = 0M;
+decimal s;
+decimal m;
+int ph;
+int pm;
+int ah;
+int am;
+decimal sh;
+decimal sm;
 
 //  EMPEZAMOS
 Console.WriteLine("Empiezo");
-var n = ns.Length;
-
+Inicializar();
 //  CICLO
+var i = 0; 
+var n = ns.Length;
 sumar:
-    x = ns[i]; //<- optimización
+    x = ns[i];                  //<- REF optimización
     s = s + x;
-    if (x < 5) goto suspendido; //<- numero mágico!!! Refactorizar
+    if (x < 5) goto suspendido; //<- REF numero mágico!!! Refactorizar
     ProcesarAprovados();
     goto sigue;
 suspendido:
     ProcesarSuspendidos();
 sigue:
     i = i + 1;
+    Console.WriteLine("en ciclo");
     if (i < n) goto sumar;
-
-
 //  FIN CICLO
 Console.WriteLine("fin ciclo");
-
-//  MEDIA
-m = s / ns.Length;
-Console.WriteLine($"participantes {n} H={ah + ph} M={am + pm}");
-// Refactorizar expresion
-Console.WriteLine($"La media es {m:0.00} H={sh / (ah + ph):00.00} M={sm / (am + pm):00.00}");
-
-// SUSPENSOS 
-Console.WriteLine($"Suspenso  H={ph} M={pm}");
-// APROVADOS 
-Console.WriteLine($"Aprobados H={ah} M={am}");
+Finalizar();
 Console.WriteLine("Fin");
 
+
+// PROCEDURE DIVISION
+void Inicializar(){
+    s = 0M;
+    m = 0M;
+    ph = 0;
+    pm = 0;
+    ah = 0;
+    am = 0;
+    sh = 0M;
+    sm = 0M;
+}
+void Finalizar(){
+    //  MEDIA
+    m = s / n;
+    Console.WriteLine($"participantes {n} H={ah + ph} M={am + pm}");
+    // Refactorizar expresion
+    Console.WriteLine($"La media es {m:0.00} H={sh / (ah + ph):00.00} M={sm / (am + pm):00.00}");
+    // SUSPENSOS 
+    Console.WriteLine($"Suspenso  H={ph} M={pm}");
+    // APROVADOS 
+    Console.WriteLine($"Aprobados H={ah} M={am}");
+}
 void ProcesarAprovados()
 {
     if (so[i] == 'M') goto AM;
@@ -78,7 +91,6 @@ void ProcesarSuspendidos()
 PM:
     ProcesarPM();
 }
-
 void ProcesarHA()
 {
     sh = sh + x;
